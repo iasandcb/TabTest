1d70a9bdde1e2986cabb187171895779f1d54091
ias <iasandcb@gmail.com>
2014-01-13 09:58:15 +0900

    Initial Commit

---
+++ TabTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabTest
+//
+//  Created by ias on 2014. 1. 13..
+//  Copyright (c) 2014년 ias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TabTest
+//
+//  Created by ias on 2014. 1. 13..
+//  Copyright (c) 2014년 ias. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
